f10880ef0459c3755b171876b6927e3347ab67bf
shibildas <shibildas@gmail.com>
2023-06-13 19:56:27 +0530

    isomorpphic

---
+++ isomorphic.js
@@ -0,0 +1,8 @@
+var isIsomorphic = function(s, t) {
+    if(s.length!==t.length) return false
+    for(let i=0;i<s.length;i++){
+    if(s.indexOf(s[i],i+1) !== t.indexOf(t[i],i+1)) return false
+    }
+    return true
+    
+};
