8751b0cc0f38cd8f6aa99ea4697c4cae2c58230a
shibildas <shibildas@gmail.com>
2023-07-17 11:30:09 +0530

    leet 2465

---
+++ 2465.js
@@ -0,0 +1,30 @@
+// 2465. Number of Distinct Averages
+// Easy
+// 299
+// 21
+// Companies
+// You are given a 0-indexed integer array nums of even length.
+
+// As long as nums is not empty, you must repetitively:
+
+// Find the minimum number in nums and remove it.
+// Find the maximum number in nums and remove it.
+// Calculate the average of the two removed numbers.
+// The average of two numbers a and b is (a + b) / 2.
+
+// For example, the average of 2 and 3 is (2 + 3) / 2 = 2.5.
+// Return the number of distinct averages calculated using the above process.
+
+// Note that when there is a tie for a minimum or maximum number, any can be removed.
+
+var distinctAverages = function(nums) {
+    var obj={}
+    nums=nums.sort((a,b)=>a-b);
+    (function checkAVG(nums){
+       if (!nums.length) return
+       let avg=(nums[0]+nums[nums.length-1])/2
+       obj[avg]? obj[avg]++: obj[avg]=1
+       checkAVG(nums.slice(1,-1))
+    })(nums)
+    return Object.keys(obj).length
+}; 
