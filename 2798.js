b49f5a84612e844ccfa19369818cf87b4eaf4df3
shibildas <shibildas@gmail.com>
2023-08-01 16:44:30 +0530

    leet 2798

---
+++ 2798.js
@@ -0,0 +1,25 @@
+// 2798. Number of Employees Who Met the Target
+// Easy
+// 88
+// 10
+// Companies
+// There are n employees in a company, numbered from 0 to n - 1. Each employee i has worked for hours[i] hours in the company.
+
+// The company requires each employee to work for at least target hours.
+
+// You are given a 0-indexed array of non-negative integers hours of length n and a non-negative integer target.
+
+// Return the integer denoting the number of employees who worked at least target hours.
+
+ 
+
+// Example 1:
+var numberOfEmployeesWhoMetTarget = function(hours, target) {
+    let count=0
+    for(let i=0;i<hours.length;i++){
+        if(hours[i]>=target){
+            count++
+        }
+    }
+    return count
+};
